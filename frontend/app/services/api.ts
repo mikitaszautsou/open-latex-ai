b84520c513ba4610f077b9677fa56c7fed0417e4
Mikita Shautsou <mikita.szautsou@gmail.com>
2025-04-11 21:25:50 +0100

    feat(frontend): Configure build.

--- frontend/app/services/api.ts
+++ frontend/app/services/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-    baseURL: "http://localhost:3000", // Your NestJS backend URL
+    baseURL: "https://open-latex-ai.onrender.com",
     headers: {
       "Content-Type": "application/json",
     },
